32270342e04e6aafd92db2432f8b0a774b20ec2f
llx <961061833@qq.com>
2020-07-05 16:52:54 +0800

    区分生产环境

--- config/util.js
+++ config/util.js
@@ -1,5 +1,19 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   resolve: (...args) => path.join(__dirname, '..', ...args),
+  baseStyleLoader: [
+    process.env.NODE_ENV === 'production'
+      ?
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            esModule: true,
+            hmr: false,
+          },
+        }
+      : 'style-loader',
+    'css-loader', 'postcss-loader'
+  ],
 };
