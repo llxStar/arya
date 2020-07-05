86f741bf268c9bfffaf3792cf8a11d0f8000be6c
llx <961061833@qq.com>
2020-07-05 17:15:26 +0800

    开发环境

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,1 +1,36 @@
+process.env.NODE_ENV = 'development';
 
+const webpack = require('webpack');
+const { resolve } = require('./util');
+const baseConfig = require('./webpack.base');
+const merge = require('webpack-merge');
+
+module.exports = merge({
+  devServer: {
+    port: 9000,
+    compress: true,
+    hot: true,
+    open: true,
+    clientLogLevel: 'silent',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: [resolve('src')],
+        options: {
+          cacheDirectory: true, // 设置了这个缓存后，动态改变.browserslistrc不会生效，生产环境禁用
+        }
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+      }
+    }),
+  ]
+}, baseConfig);
